f0ceeac408ee7f9b77854a18a430e161f86ecf90
Andrellu <andrellu.o.f@gmail.com>
2022-06-02 19:26:36 +0200

    CommitUsuarios

---
+++ TiendaVirtualXamarin/TiendaVirtualXamarin/Services/ServiceUsuarios.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Extensions.Configuration;
+using ModelsTVX;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaVirtualXamarin.Services
+{
+    public class ServiceUsuarios
+    {
+        private string UrlApi;
+        private MediaTypeWithQualityHeaderValue Header;
+
+        public ServiceUsuarios(IConfiguration configuration)
+        {
+            this.UrlApi = configuration["ApiUrl:ApiTiendaVirtual"];
+            this.Header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        public async Task<string> GetTokenAsync(string username, string password)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                LoginModel model = new LoginModel
+                {
+                    Username = username,
+                    Passwordo = password
+                };
+                string json = JsonConvert.SerializeObject(model);
+                StringContent content =
+                    new StringContent(json, Encoding.UTF8, "application/json");
+                string request = "/api/login/ValidarUsuario";
+                HttpResponseMessage response =
+                    await client.PostAsync(request, content);
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = await response.Content.ReadAsStringAsync();
+                    JObject jObject = JObject.Parse(data);
+                    string token = jObject.GetValue("response").ToString();
+                    return token;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        private async Task<T> CallApiAsync<T>(string request, string token)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                Uri uri = new Uri(this.UrlApi + request);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                client.DefaultRequestHeaders.Add("Authorization", "bearer " + token);
+                HttpResponseMessage response = await client.GetAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+
+            }
+        }
+
+        private async Task<T> CallApiAsync<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                Uri uri = new Uri(this.UrlApi + request);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                HttpResponseMessage response =
+                    await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+    }
+}
