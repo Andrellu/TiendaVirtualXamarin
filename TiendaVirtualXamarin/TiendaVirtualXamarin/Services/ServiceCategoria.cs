f1ba2adf799e1058f988829a39065ea5dd496f39
Andrellu <andrellu.o.f@gmail.com>
2022-06-02 19:39:25 +0200

    ServicesCommit

---
+++ TiendaVirtualXamarin/TiendaVirtualXamarin/Services/ServiceCategoria.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using ModelsTVX;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaVirtualXamarin.Services
+{
+    public class ServiceCategoria
+    {
+        private string UrlApi;
+        private MediaTypeWithQualityHeaderValue Header;
+
+        public ServiceCategoria(IConfiguration configuration)
+        {
+            this.UrlApi = configuration["ApiUrl:ApiTiendaVirtual"];
+            this.Header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        private async Task<T> CallApiAsync<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                Uri uri = new Uri(this.UrlApi + request);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                HttpResponseMessage response =
+                    await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+
+        public async Task<List<Categoria>> GetAllCategorias()
+        {
+            string request = "/GetCategorias";
+            List<Categoria> categorias = await this.CallApiAsync<List<Categoria>>(request);
+            return categorias;
+        }
+    }
+}
