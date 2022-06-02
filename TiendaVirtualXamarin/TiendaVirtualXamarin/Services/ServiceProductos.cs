d62d1eac97b74491f477974f5916aa42df55c2fb
Andrellu <andrellu.o.f@gmail.com>
2022-06-02 18:46:16 +0200

    comit

--- TiendaVirtualXamarin/TiendaVirtualXamarin/Services/ServiceProductos.cs
+++ TiendaVirtualXamarin/TiendaVirtualXamarin/Services/ServiceProductos.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using ModelsTVX;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -53,6 +54,81 @@ namespace TiendaVirtualXamarin.Services
             string request = "api/Productos/" + id;
             Producto producto = await this.CallApiAsync<Producto>(request);
             return producto;
+        }
+
+        public async Task<Producto> GetProductoNameAsync(string name)
+        {
+            string request = "api/Productos/GetProductoName/" + name;
+            Producto producto = await this.CallApiAsync<Producto>(request);
+            return producto;
+        }
+
+        public async Task InsertProducto(Producto producto)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "/api/Productos/InsertProducto";
+
+                Uri uri = new Uri(this.UrlApi + request);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+
+                string json = JsonConvert.SerializeObject(producto);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(request, content);
+            }
+        }
+
+        public async Task UpdateProductoAsync(Producto producto)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "/api/Productos/UpdateProducto";
+                Uri uri = new Uri(this.UrlApi + request);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                string json = JsonConvert.SerializeObject(producto);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PutAsync(request, content);
+            }
+        }
+
+        public async Task DeleteProductoAsync(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/Productos/" + id;
+                Uri uri = new Uri(this.UrlApi + request);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                HttpResponseMessage response = await client.DeleteAsync(request);
+            }
+        }
+
+        public async Task UpdateStockProductosAsync(int stock, string nombre)
+        {
+            string request = "api/Productos/UpdateStockProducto/" + stock + "/" + nombre;
+            using (HttpClient client = new HttpClient())
+            {
+                Uri uri = new Uri(this.UrlApi + request);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                HttpResponseMessage response = await client.PutAsync(request, null);
+            }
+        }
+
+        public async Task<List<Producto>> GetProductosCategoriaAsync(string categoria)
+        {
+            string request = "api/Productos/GetProductosCategoria/" + categoria;
+            List<Producto> productos = await this.CallApiAsync<List<Producto>>(request);
+            return productos;
+        }
+
+        public async Task<List<Producto>> GetProductosBuscadorAsync(string palabra)
+        {
+            string request = "api/Productos/GetProductosBuscador/" + palabra;
+            List<Producto> productos = await this.CallApiAsync<List<Producto>>(request);
+            return productos;
         }
     }
 }
