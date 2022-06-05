6d907491ab764ccbf272eb418e24182c30cef858
AgusCM <agusnegrales@hotmail.com>
2022-06-05 13:53:47 +0200

    commit

---
+++ TiendaVirtualXamarin/TiendaVirtualXamarin/Services/SessionService.cs
@@ -0,0 +1,17 @@
+﻿using ModelsTVX;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TiendaVirtualXamarin.Services
+{
+    public class SessionService
+    {
+        public List<Producto> ProductosCarrito { get; set; }        
+
+        public SessionService()
+        {
+            this.ProductosCarrito = new List<Producto>();
+        }
+    }
+}
--- TiendaVirtualXamarin/TiendaVirtualXamarin/ViewModels/PerfilViewModel.cs
+++ TiendaVirtualXamarin/TiendaVirtualXamarin/ViewModels/PerfilViewModel.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using System.Text;
 using TiendaVirtualXamarin.Base;
+using TiendaVirtualXamarin.Views;
+using Xamarin.Forms;
 
 namespace TiendaVirtualXamarin.ViewModels
 {
     public class PerfilViewModel: ViewModelBase
     {
-
+        public Command MostarCarrito
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                                        
+                });
+            }
+        }
     }
 }
