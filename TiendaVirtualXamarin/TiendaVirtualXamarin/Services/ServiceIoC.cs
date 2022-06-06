570348f8d5282202bd421ff95204f33780e8334f
AgusCM <agustincamposmartinez@hotmail.com>
2022-06-06 13:05:49 +0200

    perfilview

--- TiendaVirtualXamarin/TiendaVirtualXamarin/Services/ServiceIoC.cs
+++ TiendaVirtualXamarin/TiendaVirtualXamarin/Services/ServiceIoC.cs
@@ -36,6 +36,7 @@ namespace TiendaVirtualXamarin.Services
             builder.RegisterType<ProductosListViewModel>();
             builder.RegisterType<CarritoViewModel>();
             builder.RegisterType<ProductosCategoriaViewModel>();
+            builder.RegisterType<PerfilViewModel>();
             string resourceName = "TiendaVirtualXamarin.appsettings.json";
             Stream stream = GetType().GetTypeInfo().Assembly.GetManifestResourceStream(resourceName);
             IConfiguration configuration = new ConfigurationBuilder().AddJsonStream(stream).Build();
@@ -50,6 +51,14 @@ namespace TiendaVirtualXamarin.Services
             get
             {
                 return this.container.Resolve<SessionService>();
+            }
+        }
+
+        public PerfilViewModel PerfilViewModel
+        {
+            get
+            {
+                return this.container.Resolve<PerfilViewModel>();
             }
         }
 
