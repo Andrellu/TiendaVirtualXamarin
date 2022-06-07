32762f7571cb58edeb20cbea739f77f98d49b1e4
Rubén Pozuelo Martínez <rubenpozuelomartinez@gmail.com>
2022-06-07 11:48:48 +0200

    Modificacion final diseño

---
+++ TiendaVirtualXamarin/TiendaVirtualXamarin/Converters/DescripcionConverts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TiendaVirtualXamarin.Converters
+{
+    class DescripcionConverts:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var s = (string)value;
+            var data = char.ToUpper(s[0]) + s.Substring(1);
+            if (s.Length > 40)
+            {
+                return data.Substring(0, 40) + "...";
+            }
+            else
+            {
+                return data;
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
